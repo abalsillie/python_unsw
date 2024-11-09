912fc18992d933b63dbc53ade8b08175a68a4c22
abalsillie <abalsillie1@gmail.com>
2024-11-09 22:14:06 +1100

    round 2

--- week_2/elements.py
+++ week_2/elements.py
@@ -6,6 +6,37 @@
 # print(x[::2]) # prints every second element
 
 # adding elements
+z = "Hello"
+z = z.upper() #change to caps
+y = ['a', 'b', 'c', 'd', 'e']
+y.append('f') #appends f
+y.insert(2, 'x') #insert x at index 2
+x = ['x', 'y', 'z']
+y.extend(x) #adds 2 lists together into one
+y = x + y #same result
+print(y)
+
+# removing elements
 x = ['a', 'b', 'c', 'd', 'e']
-x.append('f') # appends f
-x = x.upper()
+del x[1] #list removes index 1, b
+del x[1:3] #removes index 1-2, b and c
+print(x.pop(2)) #removes index 2 and returns it
+x.remove('c') #removes c
+
+# modifying elements, cannot do for tuple
+x = ['a', 'b', 'c', 'd', 'e'] #list
+x[0] = 'z' # assigns index 1 a new value, a becomes z
+
+x = {1, 2, 3} #set
+x.remove(3) #removes 3 and adds 4
+x.add(4)
+
+# joining elements
+x = ['a', 'b', 'c', 'd', 'e']
+s = ','.join(x) # joins elements of a connection into a string
+print(s)
+
+# splits list into substrings
+y = 'The cat sat on the mat'
+print(y.split(' '))
+# ['The', 'cat', 'sat', 'on', 'the', 'mat']
